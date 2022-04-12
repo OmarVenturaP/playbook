1e4d08128fb2553893380f5d3d90bcf2ce69b325
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-11 22:47:27 -0500

    2. create folder example2

---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,9 @@
+// funcion info
+exports.info = (message) => {
+    console.log(`info: ${message}`)
+}
+
+// funcion verbose
+exports.verbose = (message) => {
+    console.log(`verbose: ${message}`)
+}

e8b07ebb5543bfd510d1ff83c1e95d4e2a6cf55d
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-11 22:51:54 -0500

    create folder example9

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,10 @@
+class MyPokemon {
+    constructor(name) {
+        this.name = name;
+    }
+    sayHello() {
+        console.log(`Mi Pokemón ${this.name} te saluda!`);
+    }
+}
+
+export default MyPokemon;

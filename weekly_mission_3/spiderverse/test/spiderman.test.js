0aa002047f5d7e7e6b64eade89ff3bc072d534cc
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-19 01:10:49 -0500

    agregando elementos, haciendo pruebas y corrigiendo

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,6 +1,12 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-        const resutlOfSomething = 1 + 3;
-        expect(resutlOfSomething).toBe(4);
+const Spiderman = require('../app/spiderman');
+
+describe("Unit Test for spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony");
+        expect(andrewGarfield.name).toBe("Spiderman Sony");
+        expect(andrewGarfield.age).toBe(31);
+        expect(andrewGarfield.actor).toBe("Andrew Garfield");
+        expect(andrewGarfield.movies).toBe(2);
+        expect(andrewGarfield.studio).toBe("Sony");
     })
 });

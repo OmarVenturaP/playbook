1333690d46757b304688faae43c196ab88cf13af
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-19 01:57:26 -0500

    probando undefinded dateCreate y lastUpdated

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -2,12 +2,12 @@ const User = require('../../app/models/user');
 
 describe('Unit Test for User  class', () => {
     test('Create an user object', () => {
-        const user = new User(5, "OmarVentura", "Omar Ventura", "full-Stack Jr in progress", "18/04/22", "19/04/22");
+        const user = new User(5, "OmarVentura", "Omar Ventura", "full-Stack Jr in progress");
         expect(user.id).toBe(5);
         expect(user.username).toBe("OmarVentura");
         expect(user.name).toBe("Omar Ventura");
         expect(user.bio).toBe("full-Stack Jr in progress");
-        expect(user.dateCreate).toBe("18/04/22");
-        expect(user.lastUpdate).toBe("19/04/22");
+        expect(user.dateCreate).not.toBeUndefined();
+        expect(user.lastUpdate).not.toBeUndefined();
     });
 })

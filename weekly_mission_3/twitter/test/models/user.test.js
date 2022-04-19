4db5467f599c0bf8441c1ac746d7a7fa146a1a31
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-19 01:40:58 -0500

    creando propiedades necesarias del usuario

---
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -0,0 +1,11 @@
+describe('Unit Test for User  class', () => {
+    test('Create an user objectr', () => {
+        const user = new User(5, "OmarVentura", "Omar Ventura", "full-Stack Jr in progress", "18/04/22", "19/04/22");
+        expect(user.id).toBe(5);
+        expect(user.username).toBe("OmarVentura");
+        expect(user.name).toBe("Omar Ventura");
+        expect(user.bio).toBe("full-Stack Jr in progress");
+        expect(user.dateCreate).toBe("18/04/22");
+        expect(user.lastUpdate).toBe("19/04/22");
+    });
+})

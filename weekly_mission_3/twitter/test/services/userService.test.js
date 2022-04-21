679bc81734bd2a26b40c884adacdf2b6a0b64958
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-20 23:24:20 -0500

    Sprint 2 - Paso 4) Agregando requerimiento 2 en test probar - failed (modificando nombre de clase para coincidir con el requerimiento

--- weekly_mission_3/twitter/test/services/userService.test.js
+++ weekly_mission_3/twitter/test/services/userService.test.js
@@ -1,11 +1,18 @@
-const UserServices = require('../../app/services/userService');
+const UserService = require('../../app/services/userService');
 
 describe('Test for user services', () => {
     test('1) Create a new user using the user service', () => {
-        const user = UserServices.create(5, "OmarVentura", "Omar Ventura");
+        const user = UserService.create(5, "OmarVentura", "Omar Ventura");
         expect(user.id).toBe(5);
         expect(user.username).toBe("OmarVentura");
         expect(user.name).toBe("Omar Ventura");
         expect(user.bio).not.toBeUndefined();
+    })
+    test('2. Get all use data in a list', () => {
+        const user = UserService.getInfo(user)
+        expect(userInfooInList[0]).toBe(5);
+        expect(userInfooInList[1]).toBe("OmarVentura");
+        expect(userInfooInList[2]).toBe("Omar Ventura");
+        expect(userInfooInList[3]).toBe("sin bio");
     })
 })

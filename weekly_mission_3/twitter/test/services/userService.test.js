f1db96df2cf0762205d0d2a9422b04dc5eadd22e
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-21 05:42:45 -0500

    Sprint 2 - Paso 5) Modificando algunas funciones para que el codigo se lea correctamente

--- weekly_mission_3/twitter/test/services/userService.test.js
+++ weekly_mission_3/twitter/test/services/userService.test.js
@@ -1,18 +1,19 @@
 const UserService = require('../../app/services/userService');
 
 describe('Test for user services', () => {
-    test('1) Create a new user using the user service', () => {
+    test('1. Create a new user using the user service', () => {
         const user = UserService.create(5, "OmarVentura", "Omar Ventura");
         expect(user.id).toBe(5);
         expect(user.username).toBe("OmarVentura");
         expect(user.name).toBe("Omar Ventura");
         expect(user.bio).not.toBeUndefined();
     })
-    test('2. Get all use data in a list', () => {
-        const user = UserService.getInfo(user)
-        expect(userInfooInList[0]).toBe(5);
-        expect(userInfooInList[1]).toBe("OmarVentura");
-        expect(userInfooInList[2]).toBe("Omar Ventura");
-        expect(userInfooInList[3]).toBe("sin bio");
+    test('2. Get all user data in a list', () => {
+        const user = UserService.create(5, "OmarVentura", "Omar Ventura");
+        const userInfoInList = UserService.getInfo(user);
+        expect(userInfoInList[0]).toBe(5);
+        expect(userInfoInList[1]).toBe("OmarVentura");
+        expect(userInfoInList[2]).toBe("Omar Ventura");
+        expect(userInfoInList[3]).toBe("sin bio");
     })
 })

78ca2caccef1b0c9f5b7655c19e5ee013daa603f
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-22 23:56:51 -0500

    Sprint 3 - Paso 1) Agregando test y prueba

---
+++ weekly_mission_3/twitter/test/views/userView.test.js
@@ -0,0 +1,11 @@
+const UserView = require('./../../app/views/userView');
+
+describe('Test for UserView', () => { {
+
+}
+    test('1.) Return an error object when try to create  a new user with an null payload', () => {
+        const payload = null
+        const result = UserView.createUser(payload);
+        expect(result).toMatch(/payload no existe/);
+    })
+})

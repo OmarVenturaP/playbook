b1eba1a816209ba3cf7aeab057bee0b228878869
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-22 22:55:56 -0500

    Sprint 2 - Paso 8) Agregando clase de requerimiento 3 y probando

--- weekly_mission_3/twitter/app/services/userService.js
+++ weekly_mission_3/twitter/app/services/userService.js
@@ -7,5 +7,9 @@ class UserService {
     static getInfo() {
         return [5, "OmarVentura", "Omar Ventura", "sin bio"];
     }
+    static updateUsername(user, username) {
+        user.username = username;
+    }
 }
+
 module.exports = UserService;

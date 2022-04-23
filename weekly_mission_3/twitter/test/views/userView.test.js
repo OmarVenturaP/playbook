a6801a95dc991e3aa9abe7e050e5983960ff9913
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-23 01:48:20 -0500

    Sprint 3 - Paso 5) Agregando test del requerimiento 3 y prueba

--- weekly_mission_3/twitter/test/views/userView.test.js
+++ weekly_mission_3/twitter/test/views/userView.test.js
@@ -3,14 +3,19 @@ const UserView = require('./../../app/views/userView');
 describe('Test for UserView', () => { {
 
 }
-    test('1.) Return an error object when try to create  a new user with an null payload', () => {
+    test('1.) Return an error object when try to create a new user with an null payload', () => {
         const payload = null
         const result = UserView.createUser(payload);
         expect(result.error).toMatch(/payload no existe/);
     })
-    test('2.) Return an error object when try to create  a new user with an empty payload', () => {
+    test('2.) Return an error object when try to create a new user with a payload with invalid propierties', () => {
         const payload = {username: null, name: 'Omar', id: 'id'}
         const result = UserView.createUser(payload);
+        expect(result.error).toMatch(/necesitan tener un valor válido/);
+    })
+    test('3.) Return an error object when try to create a new user with missing propierties', () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor válido/);
     })
 })

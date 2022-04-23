1c934676c2141df55d3432faf8c2c8a3d65e1df9
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-22 23:02:12 -0500

    Sprint 2 - Paso 9) - Agregando test de Requerimiento 4 y probando

--- weekly_mission_3/twitter/test/services/userService.test.js
+++ weekly_mission_3/twitter/test/services/userService.test.js
@@ -21,4 +21,13 @@ describe('Test for user services', () => {
         UserService.updateUsername(user, "OVentura");
         expect(user.username).toBe("OVentura");
     })
+    test('4. Given a list of users give me the list of usernames', () => {
+        const user1 = UserService.create(5, "OmarVentura1", "Omar");
+        const user2 = UserService.create(5, "OmarVentura2", "Omar");
+        const user3 = UserService.create(5, "OmarVentura3", "Omar");
+        const usernames = UserService.getUsernames([user1, user2, user3]);
+        expect(usernames).toBe("OmarVentura1");
+        expect(usernames).toBe("OmarVentura2");
+        expect(usernames).toBe("OmarVentura3");
+    })
 })

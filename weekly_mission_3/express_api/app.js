3be19146c9e22da2bd620b2da69586bad5537abb
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-04-26 00:52:41 -0500

    API - Paso 5) Agregando endpoint query params para llamar un explorer por id

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -6,13 +6,19 @@ app.use(express.json())
 const port = 3000
 
 app.get('/v1/explorers', (req, res) => {
-    console.log('API Explorer GET ALL REQUEST');
+    console.log(`API Explorer GET ALL REQUEST ${new Date()}`);
     const explorer1 = {id: 1, name: 'Omar1'}
     const explorer2 = {id: 2, name: 'Omar2'}
     const explorer3 = {id: 3, name: 'Omar3'}
     const explorer4 = {id: 4, name: 'Omar4'}
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
+})
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`API Explorer GET REQUEST ${new Date()}`)
+    console.log(`API Explorer GET REQUEST ${req.params.id}`)
+    const explorer = {id: 1, name: 'Omar1'}
+    res.status(200).json(explorer)
 })
 
 app.listen(port, () => {
